484c1d35c1892a5fba9139cd7a64860083a69a8e
dnybhure <32614861+dnybhure@users.noreply.github.com>
2023-01-25 02:05:43 +0530

    2 files modified

--- src/app/start-doing/start-doing.component.ts
+++ src/app/start-doing/start-doing.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-start-doing',
   templateUrl: './start-doing.component.html',
-  styleUrls: ['./start-doing.component.css']
+  styleUrls: ['./start-doing.component.css'],
 })
 export class StartDoingComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public startDoingForm: FormGroup;
+  constructor(private startDoingfb: FormBuilder) {
+    this.startDoingForm = startDoingfb.group({
+      startDoingItems: startDoingfb.array([]),
+    });
   }
 
+  ngOnInit() {}
+  get startDoingItems(): FormArray {
+    return this.startDoingForm.get('startDoingItems') as FormArray;
+  }
+
+  newStartDoingNotes(): FormGroup {
+    return this.startDoingfb.group({
+      startDoing: '',
+    });
+  }
+  removestartDoingNotes(i: number) {
+    this.startDoingItems.removeAt(i);
+  }
+  addStartDoingNotes() {
+    this.startDoingItems.push(this.newStartDoingNotes());
+    console.log(this.startDoingItems.length);
+    setTimeout(() => {
+      document
+        .getElementById(`startDoing${this.startDoingItems.length - 1}`)
+        .focus();
+    }, 100);
+  }
 }

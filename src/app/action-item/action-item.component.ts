8e8f217aa43a9e52bc4f500467e69e47612b637d
dnybhure <32614861+dnybhure@users.noreply.github.com>
2023-01-25 01:51:55 +0530

    3 files modified

--- src/app/action-item/action-item.component.ts
+++ src/app/action-item/action-item.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-action-item',
   templateUrl: './action-item.component.html',
-  styleUrls: ['./action-item.component.css']
+  styleUrls: ['./action-item.component.css'],
 })
 export class ActionItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public actionItemForm: FormGroup;
+  constructor(private actionItemfb: FormBuilder) {
+    this.actionItemForm = actionItemfb.group({
+      actionItems: actionItemfb.array([]),
+    });
   }
 
+  ngOnInit() {}
+  get actionItems(): FormArray {
+    return this.actionItemForm.get('actionItems') as FormArray;
+  }
+
+  newActionItemNotes(): FormGroup {
+    return this.actionItemfb.group({
+      actionItem: '',
+    });
+  }
+  removeActionItemNotes(i: number) {
+    this.actionItems.removeAt(i);
+  }
+  addActionItemNotes() {
+    this.actionItems.push(this.newActionItemNotes());
+    console.log(this.actionItems.length);
+    setTimeout(() => {
+      document
+        .getElementById(`actionItem${this.actionItems.length - 1}`)
+        .focus();
+    }, 100);
+  }
 }
